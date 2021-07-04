662f1d0c310a33061666827f73afeb5600e7b385
faceless49 <digital.zeus@mail.ru>
2021-07-04 17:21:33 +0300

    Updates

---
+++ precache-manifest.cac32bc995c7ed266b2ce10e4cf3b6aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ae40dcbc2424d44f7a54d0d46ceb81",
+    "url": "/diff-tasks/index.html"
+  },
+  {
+    "revision": "bf0dfd1ed1c6419e0892",
+    "url": "/diff-tasks/static/css/main.2c4aecad.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/diff-tasks/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/diff-tasks/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf0dfd1ed1c6419e0892",
+    "url": "/diff-tasks/static/js/main.44e3f0de.chunk.js"
+  },
+  {
+    "revision": "c34e434a938a7993d8fc",
+    "url": "/diff-tasks/static/js/runtime-main.74605055.js"
+  }
+]);
