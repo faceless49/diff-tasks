a1481da0ffcde6987a775b8dd31a6fcbed57b6eb
faceless49 <digital.zeus@mail.ru>
2021-07-05 23:01:24 +0300

    try to add font awesome

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -4,37 +4,36 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
 
 function HW6() {
-    const [value, setValue] = useState<string>('')
+  const [value, setValue] = useState<string>('')
 
-    const save = () => {
-        saveState<string>('editable-span-value', value)
-    }
-    const restore = () => {
-        // setValue()
-    }
+  const save = () => {
+    saveState<string>('editable-span-value', value)
+  }
+  const restore = () => {
+    setValue(restoreState('editable-span-value', value))
+  }
 
-    return (
-        <div>
-            <hr/>
-            homeworks 6
+  return (
+    <div>
+      <hr/>
+      homeworks 6
 
-            {/*should work (должно работать)*/}
-            <div>
-                <SuperEditableSpan
-                    value={value}
-                    onChangeText={setValue}
-                    spanProps={{children: value ? undefined : 'enter text...'}}
-                />
-            </div>
-            <SuperButton onClick={save}>save</SuperButton>
-            <SuperButton onClick={restore}>restore</SuperButton>
+      <div>
+        <SuperEditableSpan
+          value={value}
+          onChangeText={setValue}
+          spanProps={{children: value ? undefined : 'enter text...'}}
+        />
+      </div>
+      <SuperButton onClick={save}>save</SuperButton>
+      <SuperButton onClick={restore}>restore</SuperButton>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperEditableSpan/>*/}
-            <hr/>
-        </div>
-    )
+      <hr/>
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeSuperEditableSpan/>*/}
+      <hr/>
+    </div>
+  )
 }
 
 export default HW6
