1dd1645cea29478e26f7bf38834e57bf553c3962
faceless49 <digital.zeus@mail.ru>
2021-07-14 22:24:51 +0300

    HW7 done

--- src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -3,28 +3,33 @@ import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react
 type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
 
 type SuperSelectPropsType = DefaultSelectPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
+  options?: any[]
+  onChangeOption?: (option: any) => void
 }
 
 const SuperSelect: React.FC<SuperSelectPropsType> = (
-    {
-        options,
-        onChange, onChangeOption,
-        ...restProps
-    }
+  {
+    options,
+    onChange, onChangeOption,
+    ...restProps
+  }
 ) => {
-    const mappedOptions: any[] = []; // map options with key
+  const mappedOptions: any[] = [options?.map((o, i) => {
+    return <option key={o + '-' + 1}>{o}</option>
+  })]; // map options with key
 
-    const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        // onChange, onChangeOption
-    }
+  const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
 
-    return (
-        <select onChange={onChangeCallback} {...restProps}>
-            {mappedOptions}
-        </select>
-    )
+    onChange && onChange(e) // ! Repeat this with SuperInputText.
+    onChangeOption && onChangeOption(e.currentTarget.value)
+    // onChange, onChangeOption
+  }
+
+  return (
+    <select onChange={onChangeCallback} {...restProps}>
+      {mappedOptions}
+    </select>
+  )
 }
 
 export default SuperSelect
