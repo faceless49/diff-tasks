88b45895f72df5732aca516c01496b91649d05fe
faceless49 <digital.zeus@mail.ru>
2021-07-25 22:28:59 +0300

    make HW8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,23 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
-        }
-        case 'check': {
-            // need to fix
-            return state
-        }
-        default: return state
+import {UserType} from '../HW8';
+
+
+type ActionType = { type: 'sort', payload: 'up' | 'down' } | { type: 'check', payload: number }
+
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+  switch (action.type) {
+    case 'sort': {
+      let stateCopy = [...state].sort((a, b) => {
+        if (a.name > b.name) return 1
+        else if (a.name < b.name) return -1
+        else return 0
+      })
+
+      return action.payload === 'up' ? stateCopy : stateCopy.reverse();
     }
+    case 'check':
+      return state.filter(a => a.age >= action.payload)
+    default:
+      return state
+  }
 }
